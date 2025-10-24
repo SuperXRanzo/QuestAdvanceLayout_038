a5a8a9f199df24749d3debcf3a52531a8ada71d2
SuperXRanzo <okanskyavenger@gmail.com>
2025-10-24 20:00:16 +0700

    menambahkan Image untuk profilescreen

--- app/src/main/java/com/example/composablelayout2/UIkedua.kt
+++ app/src/main/java/com/example/composablelayout2/UIkedua.kt
@@ -1,13 +1,20 @@
 package com.example.composablelayout2
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -19,5 +26,14 @@ fun ProfileScreen() {
             .padding(horizontal = 20.dp, vertical = 40.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_pic),
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+        )
 
+        Spacer(modifier = Modifier.height(12.dp))
+    }
     }
