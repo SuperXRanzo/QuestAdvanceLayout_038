eb7fa796e6b4e1701b53485217dc505784fe5262
SuperXRanzo <okanskyavenger@gmail.com>
2025-10-24 19:58:45 +0700

    menambahkan ProfileScreen

--- app/src/main/java/com/example/composablelayout2/UIkedua.kt
+++ app/src/main/java/com/example/composablelayout2/UIkedua.kt
@@ -1,2 +1,23 @@
 package com.example.composablelayout2
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProfileScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF101010))
+            .padding(horizontal = 20.dp, vertical = 40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
